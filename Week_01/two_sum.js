8a3db1c83f4f472050e4fc823d4e17036fe25385
luankefei <luankefei@gmail.com>
2020-05-19 12:43:43 +0800

    leetcode 1 two sum

--- Week_01/two_sum.js
+++ Week_01/two_sum.js
@@ -17,5 +17,18 @@ var twoSum = function (nums, target) {
   }
 };
 
+// 利用哈希存储 空间换时间，空间O(n)，时间O(n)
+var twoSum2 = function (nums, target) {
+  const valueMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (valueMap[nums[i]] !== undefined) {
+      return [valueMap[nums[i]], i];
+    }
+
+    valueMap[target - nums[i]] = i;
+  }
+};
+
 // 测试
 console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum2([2, 7, 11, 15], 9));
