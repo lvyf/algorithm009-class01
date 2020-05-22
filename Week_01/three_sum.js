f5bb12fbc32bf590ee8e641b6c1c8d81bd01f499
luankefei <luankefei@gmail.com>
2020-05-22 21:44:02 +0800

    three sum

--- Week_01/three_sum.js
+++ Week_01/three_sum.js
@@ -8,36 +8,35 @@
 // 无论暴力法，hash法还是双指针法，主要困难在去重
 
 // 双指针夹逼法
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var threeSum = function (nums) {
+  if (nums.length < 3) return [];
+
+  nums.sort((a, b) => a - b);
   const result = [];
 
-  // step 1: 夹逼法必须先排序
-  nums.sort((a, b) => a - b);
-
-  // 寻找每一个位置上的k
   for (let k = 0; k < nums.length - 2; k++) {
-    if (nums[k] > 0) break; // # 1. because of j > i > k.
-    if (k > 0 && nums[k] === nums[k - 1]) continue; // 2. skip the same `nums[k]`
+    if (nums[k] > 0) break;
+    if (k > 0 && nums[k] === nums[k - 1]) continue;
 
     let i = k + 1;
     let j = nums.length - 1;
 
     while (i < j) {
-      const s = nums[k] + nums[i] + nums[j];
+      const sum = nums[k] + nums[i] + nums[j];
 
-      if (s < 0) {
-        i++;
-        while (i < j && nums[i] === nums[i - 1]) i++;
-      } else if (s > 0) {
-        j--;
-        while (i < j && nums[j] === nums[j + 1]) j--;
-        // equal 三数之和为0
+      if (sum < 0) {
+        while (i < j && nums[++i] === nums[i - 1]) continue;
+      } else if (sum > 0) {
+        while (i < j && nums[--j] === nums[j + 1]) continue;
       } else {
         result.push([nums[i], nums[j], nums[k]]);
         i++;
         j--;
 
-        // 接下来这里要做去重，注意这里的i和j都已经是下一个元素了
         while (i < j && nums[i] === nums[i - 1]) i++;
         while (i < j && nums[j] === nums[j + 1]) j--;
       }
