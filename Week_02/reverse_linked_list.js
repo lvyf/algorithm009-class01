60295e3a216c41ae18f0e9e4c79211a63858a752
luankefei <luankefei@gmail.com>
2020-05-25 14:08:48 +0800

    leetcode 206 reverse list

---
+++ Week_02/reverse_linked_list.js
@@ -0,0 +1,37 @@
+/**
+ * 206. 反转链表
+ */
+var reverseList = function (head) {
+  if (!head) return head;
+  let prev = head;
+
+  while (head.next) {
+    const temp = prev;
+    prev = head.next;
+    head.next = head.next.next;
+    prev.next = temp;
+  }
+
+  return prev;
+};
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function createLinkedList(nums) {
+  let list = new ListNode(null);
+  const temp = list;
+  nums.forEach((item) => {
+    list.next = new ListNode(item);
+    list = list.next;
+  });
+
+  return temp;
+}
+
+// test
+const l1 = createLinkedList([1, 2, 3, 4, 5]);
+const l2 = reverseList(l1);
+console.log(l2);
